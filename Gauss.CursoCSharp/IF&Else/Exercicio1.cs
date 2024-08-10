e9c78ad6235d124568070c2f75d053b624d85c36
Nilton <hajikazuo@outlook.com>
2024-08-10 10:24:12 -0300

    aula3

---
+++ Gauss.CursoCSharp/IF&Else/Exercicio1.cs
@@ -0,0 +1,18 @@
+﻿//Crie uma lista de números e verifique se são positivos, negativos ou neutros (zero).  
+var numeros = new List<int> { 5, -3, 0, 7, -2, 8, -1 };
+
+foreach (var numero in numeros)
+{
+    if (numero > 0)
+    {
+        Console.WriteLine($"Número positivo:{numero}");
+    }
+    else if (numero < 0)
+    {
+        Console.WriteLine($"Número negativo:{numero}");
+    }
+    else
+    {
+        Console.WriteLine($"Número neutro:{numero}");
+    }
+}
---
+++ Gauss.CursoCSharp/IF&Else/IF&ElseExemplos.cs
@@ -0,0 +1,81 @@
+﻿//Tela de Login
+var usuarios = new List<string> { "João", "Maria", "José", "Ana", "Nilton" };
+
+Console.WriteLine("Bem vindo! Para acessar nosso sistema, por favor, digite seu nome de usuário ");
+Console.Write("Login: ");
+string nomeUsuario = Console.ReadLine();
+
+if (nomeUsuario == "")
+{
+    Console.WriteLine("Usuário não informado, por favor, tente novamente.");
+}
+else if (usuarios.Contains(nomeUsuario))
+{
+    Console.WriteLine($"Usuário {nomeUsuario} logado com sucesso.");
+}
+else
+{
+    Console.WriteLine("Usuário não cadastrado, por favor, verifique o nome e tente novamente.");
+}
+
+// Criação de um sistema de login
+var usuarios = new List<string> { "João", "Maria", "José", "Ana", "Nilton" };
+
+Console.WriteLine("Bem vindo! Para acessar nosso sistema, por favor, digite seu nome de usuário ");
+Console.Write("Login: ");
+string nomeUsuario = Console.ReadLine();
+bool usuarioLogado = false;
+
+if (usuarios.Contains(nomeUsuario))
+{
+    Console.WriteLine($"Usuário {nomeUsuario} logado com sucesso.");
+    usuarioLogado = true;
+}
+else if (nomeUsuario == "")
+{
+    Console.WriteLine("Usuário não informado, por favor, tente novamente.");
+}
+else
+{
+    Console.WriteLine("Usuário não cadastrado, por favor, verifique o nome e tente novamente.");
+}
+
+// Sistema de caixa eletrônico
+
+
+if (usuarioLogado)
+{
+    Console.WriteLine("Escolha uma opção: ");
+    Console.WriteLine("\n 1 - Verificar meu saldo\n 2 - Sacar\n 3 - Depositar \n");
+    int opcao = int.Parse(Console.ReadLine());
+    decimal saldo = 1000;
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine($"Seu saldo é de {saldo:C}");
+            break;
+        case 2:
+            Console.Write("Digite o valor que deseja sacar: ");
+            var valorSaque = decimal.Parse(Console.ReadLine());
+            if (valorSaque > saldo)
+            {
+                Console.WriteLine("Saldo insuficiente.");
+            }
+            else
+            {
+                saldo -= valorSaque;
+                Console.WriteLine($"Saque realizado com sucesso. Seu saldo atual é de {saldo:C}");
+            }
+            break;
+        case 3:
+            Console.WriteLine("Digite o valor que deseja depositar: ");
+            var valorDeposito = decimal.Parse(Console.ReadLine());
+            saldo += valorDeposito;
+            Console.WriteLine($"Depósito realizado com sucesso. Seu saldo atual é de {saldo:C}");
+            break;
+        default:
+            Console.WriteLine("Opção inválida, por favor, tente novamente.");
+            break;
+    }
+
+}
