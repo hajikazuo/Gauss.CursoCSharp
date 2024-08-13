f279c24ee6d197a8efa203a02d1b1431c29415c4
hajikazuo <hajikazuo@outlook.com>
2024-08-13 13:41:36 -0300

    fix

--- Gauss.CursoCSharp/IF&Else/Exercicio2.cs
+++ Gauss.CursoCSharp/IF&Else/Exercicio2.cs
@@ -1,20 +1,28 @@
-﻿Console.WriteLine("Digite a nota do aluno");
-int nota = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Digite a nota do produto (1 a 5): ");
+int nota = Convert.ToInt32(Console.ReadLine());
 
+string avaliacao;
 
-if (nota >= 90)
+switch (nota)
 {
-    Console.WriteLine("Excelente");
+    case 1:
+        avaliacao = "Muito ruim";
+        break;
+    case 2:
+        avaliacao = "Ruim";
+        break;
+    case 3:
+        avaliacao = "Regular";
+        break;
+    case 4:
+        avaliacao = "Bom";
+        break;
+    case 5:
+        avaliacao = "Excelente";
+        break;
+    default:
+        avaliacao = "Nota inválida";
+        break;
 }
-else if (nota >= 70)
-{
-    Console.WriteLine("Bom");
-}
-else if (nota >= 50)
-{
-    Console.WriteLine("Regular");
-}
-else
-{
-    Console.WriteLine("Insuficiente");
-}
+
+Console.WriteLine($"Avaliação: {avaliacao}");
