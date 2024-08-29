607f588a536a88f1f96d88aae2c30df666022999
hajikazuo <hajikazuo@outlook.com>
2024-08-29 17:25:50 -0300

    feat desafio1

---
+++ Gauss.CursoCSharp/LacosRepeticao/Desafio1.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"\n");
+
+for (int i = 1; i <= numero; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(j + " ");
+    }
+    Console.WriteLine();
+}
---
+++ Gauss.CursoCSharp/LacosRepeticao/Exercicio1.cs
@@ -0,0 +1,23 @@
+﻿//Crie a tabuada do 1 ao 10, separe cada tabuada com um texto.
+
+for (int numero = 1; numero <= 10; numero++)
+{
+    Console.WriteLine($"\nTabuada do {numero}:\n");
+
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine($"{numero} x {i} = {numero * i}");
+    }
+}
+
+//Fazer a tabuada ao contrário.
+
+for (int numero = 1; numero <= 10; numero++)
+{
+    Console.WriteLine($"\nTabuada do {numero}:\n");
+
+    for (int i = 10; i >= 0; i--)
+    {
+        Console.WriteLine($"{numero} x {i} = {numero * i}");
+    }
+}
